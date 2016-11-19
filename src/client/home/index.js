a5131aed0d53d24eccaf2291611a448dbab3ced4
Romel Pérez <ronelprhone@gmail.com>
2016-11-19 01:38:19 -0500

    [update] home

--- src/client/home/index.js
+++ src/client/home/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React      from 'react';
 import { render } from 'react-dom';
-import { Row, Column } from 'react-foundation';
-
-const root = document.querySelector('#app');
+import { Row }    from 'react-foundation';
+import i18n       from 'shared/i18n';
+import Container  from 'client/components/main/Container';
+import Header     from 'client/components/main/Header';
+import Content    from 'client/components/main/Content';
+import Footer     from 'client/components/main/Footer';
 
 function App () {
+
+  const title = i18n.t('home.title');
+
   return (
-    <main className='home'>
-      <Row>
-        <Column small={12}>
-          <h1>Home</h1>
-        </Column>
-        <Column small={12}>
-          <p>Home Application Site.</p>
-        </Column>
-      </Row>
-    </main>
+    <Container className='home'>
+      <Header>
+        {title}
+      </Header>
+      <Content>
+        <Row isColumn={true}>
+          <p>Home site long content used as principal entry in web application.</p>
+          <p><a href='/login'><b>Login</b></a> or <a href='/app'><b>App</b></a>.</p>
+        </Row>
+      </Content>
+      <Footer />
+    </Container>
   );
 }
+
+const root = document.querySelector('#app');
 
 render(<App />, root);
