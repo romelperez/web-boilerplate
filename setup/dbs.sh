908cdfae602118e815d3a480cb88fb79c623b328
Romel Pérez <ronelprhone@gmail.com>
2017-07-28 15:44:53 -0500

    [refactor] chore

--- setup/dbs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mongo < ./setup/mongodb/admin.js
-mongo < ./setup/mongodb/base.js
-mongo < ./setup/mongodb/users.js
--- setup/mongodb/admin.js
+++
@@ -1,7 +0,0 @@
-use admin;
-
-db.createUser({
-  user: 'superadmin',
-  pwd: '12345678',
-  roles: [ 'root' ]
-});
